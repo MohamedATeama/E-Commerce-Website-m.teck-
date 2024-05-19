28d14c5e2af5cd5cf93df9b21545703a5b062f54
Mohamed Teama <125480953+MohamedATeama@users.noreply.github.com>
2024-05-20 00:33:39 +0300

    Update user.js

--- js/user.js
+++ js/user.js
@@ -3,10 +3,9 @@ let user_info = document.getElementById("user-info");
 let user = document.getElementById("user");
 let logout = document.getElementById("logout");
 
-if(localStorage.getItem("email")) {
+if(localStorage.getItem("fname")) {
     sign.remove();
-    email = localStorage.getItem("email");
-    username = email.split("@")[0];
+    username = localStorage.getItem("fname");
     user.innerHTML = `Welcome, ${username}`;
 } else {
     user_info.remove();
